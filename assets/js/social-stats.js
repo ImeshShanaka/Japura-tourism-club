910d838152dc1154152a37ce6e14afc1a7706369
Imesh Shanaka <167472638+ImeshShanaka@users.noreply.github.com>
2025-12-17 02:42:53 +0530

    First_commit
    Date: 17.12.2025
    Time: 02:42 AM

---
+++ assets/js/social-stats.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    facebook: {
+        accessToken: 'YOUR_ACTUAL_FB_ACCESS_TOKEN',
+        pageId: 'YOUR_ACTUAL_FB_PAGE_ID'
+    },
+    youtube: {
+        apiKey: 'YOUR_ACTUAL_YOUTUBE_API_KEY',
+        channelId: 'YOUR_ACTUAL_YOUTUBE_CHANNEL_ID'
+    }
+};
